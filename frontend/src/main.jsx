30cc5726981a3ff1be105f1b3a8f91b5fb43b04a
nishantHolla <nishantholla19@gmail.com>
2024-10-05 23:28:29 +0530

    Change folder structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Signup from './Signup'
-import Login from './Login'
-import Home from './Home'
+import './styles/index.css'
+import './styles/style.css'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Home from './pages/Home'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function rout() {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./style.css"; 
 import { Link } from "react-router-dom";
 
 function Login() {
