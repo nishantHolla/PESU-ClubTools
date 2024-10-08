c7d7791523997d627635e423818ceb88a62fde4b
nishantHolla <nishantholla19@gmail.com>
2024-10-08 11:48:19 +0530

    Add fonts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./styles/colors.css"
+import "./styles/fonts.css"
 
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
