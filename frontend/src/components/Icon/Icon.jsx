45a736a10a13610d863b85602257743f22023af7
nishantHolla <nishantholla19@gmail.com>
2024-10-09 20:37:03 +0530

    Update Icon component

--- frontend/src/components/Icon/Icon.jsx
+++ frontend/src/components/Icon/Icon.jsx
@@ -24,9 +24,9 @@ function Icon({ type }) {
         <path
           fill="none"
           stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M5 13a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2zm3-2V7a4 4 0 1 1 8 0v4m-1 5h.01m-3 0h.01m-3 0h.01"
         />
       </svg>
@@ -42,9 +42,9 @@ function Icon({ type }) {
         <g
           fill="none"
           stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
         >
           <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0" />
           <path d="M21 12q-3.6 6-9 6t-9-6q3.6-6 9-6t9 6" />
@@ -62,9 +62,9 @@ function Icon({ type }) {
         <g
           fill="none"
           stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
         >
           <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
           <path d="M16.681 16.673A8.7 8.7 0 0 1 12 18q-5.4 0-9-6q1.908-3.18 4.32-4.674m2.86-1.146A9 9 0 0 1 12 6q5.4 0 9 6q-1 1.665-2.138 2.87M3 3l18 18" />
