29454910cbe49578517dce5575eb694387d0d27b
Pranavh-2004 <pranavh2004@gmail.com>
2024-10-09 11:32:58 +0530

    Updated icons and home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Button from "../components/Button/Button";
 import Input from "../components/Input/Input";
+import Password from "../components/Password/Password";
 
 function Home() {
   return (
     <div>
-      <h1>This is the home page</h1>
+      <h1>This is the home page!</h1>
       {/* <Button>text</Button> */}
       <Input placeholder="Username" icon="user" />
-      <Input placeholder="Password" icon="password" />
+      <Password />
       <Button>Click Me</Button>
     </div>
   );
