d273496bc39d15470b4a8517298e019600da6ba7
nishantHolla <nishantholla19@gmail.com>
2024-10-09 11:12:17 +0530

    Change home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Input from "../components/Input/Input";
 function Home() {
   return (
     <div>
-      <h1>This is the home page</h1>
+      <h1>This is the home page!</h1>
       {/* <Button>text</Button> */}
       <Input placeholder="Username" icon="user" />
       <Input placeholder="Password" icon="password" />
