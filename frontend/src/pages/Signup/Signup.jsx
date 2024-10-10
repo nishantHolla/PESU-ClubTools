fd84ebbab5bf8f4af8f748a1774587afa8d97247
Pranavh-2004 <pranavh2004@gmail.com>
2024-10-10 21:52:01 +0530

    Added components to signup page

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,15 +1,22 @@
 import Box from "../../components/Box/Box";
 import Input from "../../components/Input/Input";
 import Password from "../../components/Password/Password";
+import Button from "../../components/Button/Button";
 import "./signup_style.css";
 
 function Signup() {
   return (
     <div className="signup-container">
       <Box className="signup-box">
+        <h2>Join us today!</h2>
+        <p>Signup now to become a member</p>
         <Input icon="mdi:user" placeholder="Clubname"></Input>
         <Input icon="mdi:mail" placeholder="Email"></Input>
-        <Password />
+        <Input icon="mdi:password" placeholder="Password"></Input>
+        <Password placeholder="Re-enter password" />
+        <Button>Signup</Button>
+        <p>Already a member? Login here</p>
+        <Button>Signup with Google</Button>
       </Box>
     </div>
   );
