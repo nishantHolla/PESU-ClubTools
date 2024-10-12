e0a70c94de401888f6454b2c759f58fedef86b56
nishantHolla <nishantholla19@gmail.com>
2024-10-12 14:27:02 +0530

    Update home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import Button from "../components/Button/Button";
-import Input from "../components/Input/Input";
-import Password from "../components/Password/Password";
-import Icon from "../components/Icon/Icon";
 
 function Home() {
   return (
     <div>
       <h1>This is the home page!</h1>
-      <Password />
     </div>
   );
 }
