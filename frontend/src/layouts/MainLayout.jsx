edc8edb8716f511e053a7975e8958c65eddfdf6a
nishantHolla <nishantholla19@gmail.com>
2024-11-10 23:01:45 +0530

    Update background

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,7 +4,9 @@ import { Outlet } from "react-router-dom";
 function MainLayout() {
   return (
     <div className="main-layout">
-      <div className="background-container"></div>
+      <div className="background-gradient"></div>
+      <div className="background"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <header>
         <NavBar />
       </header>
